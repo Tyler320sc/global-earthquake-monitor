2c40db12bdd1ac4a07ce975523ff75afff37d6db
Tyler Carroll <typrograms.238@gmail.com>
2021-10-20 11:30:30 -0400

    added map component

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -19,34 +19,24 @@ const Map = compose(
   withScriptjs,
   withGoogleMap
 )((props) => (
-  <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
+  <GoogleMap
+    defaultZoom={2.5}
+    defaultCenter={{ lat: 40.73061, lng: -73.935242 }}
+  >
     {props.isMarkerShown && (
       <Marker position={{ lat: -34.397, lng: 150.644 }} />
     )}
+    <Circle
+      center={{ lat: 40.73061, lng: -73.935242 }}
+      radius={100000}
+      options={{
+        strokeColor: `red`,
+        fillColor: `red`,
+        fillOpacity: 0.2,
+        strokeWeight: 0,
+      }}
+    />
   </GoogleMap>
 ));
-
-// const Map = () => {
-//   return (
-//     <GoogleMap
-//       defaultZoom={2.5}
-//       defaultCenter={{ lat: 40.73061, lng: -73.935242 }}
-//     >
-//       {props.isMarkerShown && (
-//         <Marker position={{ lat: 40.7128, lng: 74.006 }} />
-//       )}
-//       <Circle
-//         center={{ lat: 40.73061, lng: -73.935242 }}
-//         radius={100000}
-//         options={{
-//           strokeColor: `red`,
-//           fillColor: `red`,
-//           fillOpacity: 0.2,
-//           strokeWeight: 0,
-//         }}
-//       />
-//     </GoogleMap>
-//   );
-// };
 
 export default Map;
