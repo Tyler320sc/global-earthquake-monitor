5d120fb9c557ad73315f97075c9a9f872455ca6d
Tyler Carroll <typrograms.238@gmail.com>
2021-11-07 20:54:20 -0500

    added more info to Recent.js and EarthquakeDetails.js

--- src/components/EarthquakeDetails.js
+++ src/components/EarthquakeDetails.js
@@ -1,13 +1,18 @@
 import React from "react";
+import moment from "moment";
 
 const EarthquakeDetails = ({ earthquake }) => {
-  //   console.log(`earthquakeData`, earthquakeData);
+  const timeOfQuake = moment(earthquake.time).format(
+    "dddd, MMMM Do YYYY, h:mm:ss a"
+  );
+
   return (
     <section>
       <h1>Earthquake Details</h1>
-      <h4>
-        {earthquake.place} {earthquake.magnitude} magnitude
-      </h4>
+      <h3>Place - {earthquake.place}</h3>
+      <h3>Magnitude - {earthquake.magnitude}</h3>
+      <h3>Time - {timeOfQuake}</h3>
+      <h3>Was it felt? {earthquake.felt ? "Yes" : "No"}</h3>
     </section>
   );
 };
--- src/components/Recent.js
+++ src/components/Recent.js
@@ -1,12 +1,19 @@
 import React from "react";
+import moment from "moment";
 
 const Recent = ({ firstEarthquake }) => {
+  const timeOfQuake = moment(firstEarthquake.time).format(
+    "dddd, MMMM Do YYYY, h:mm:ss a"
+  );
+  console.log(timeOfQuake);
+
   return (
     <section>
       <h1>Most Recent Earthquake</h1>
-      <h4>
-        {firstEarthquake.place} {firstEarthquake.magnitude} magnitude
-      </h4>
+      <h3>Place - {firstEarthquake.place}</h3>
+      <h3>Magnitude - {firstEarthquake.magnitude}</h3>
+      <h3>Time - {timeOfQuake}</h3>
+      <h3>Was it felt? {firstEarthquake.felt ? "Yes" : "No"}</h3>
     </section>
   );
 };
